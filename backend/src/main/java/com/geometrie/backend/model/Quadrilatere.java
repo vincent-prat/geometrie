c7c244c0309c7174912e0af5089245f696ac6f06
Vincent PRAT <prat.vincent.romain@gmail.com>
2022-03-14 15:55:14 +0100

    Initial commit

--- backend/src/main/java/com/geometrie/backend/model/Quadrilatere.java
+++ backend/src/main/java/com/geometrie/backend/model/Quadrilatere.java
@@ -6,4 +6,49 @@ public class Quadrilatere {
     private double coteC;
     private double coteD;
 
+    public Quadrilatere() {
+    }
+
+    public Quadrilatere(double coteA, double coteB, double coteC, double coteD) {
+        this.coteA = coteA;
+        this.coteB = coteB;
+        this.coteC = coteC;
+        this.coteD = coteD;
+    }
+
+    public double getCoteA() {
+        return coteA;
+    }
+
+    public void setCoteA(double coteA) {
+        this.coteA = coteA;
+    }
+
+    public double getCoteB() {
+        return coteB;
+    }
+
+    public void setCoteB(double coteB) {
+        this.coteB = coteB;
+    }
+
+    public double getCoteC() {
+        return coteC;
+    }
+
+    public void setCoteC(double coteC) {
+        this.coteC = coteC;
+    }
+
+    public double getCoteD() {
+        return coteD;
+    }
+
+    public void setCoteD(double coteD) {
+        this.coteD = coteD;
+    }
+
+    public void paint(String color){
+        System.out.println("je suis en "+color);
+    }
 }
