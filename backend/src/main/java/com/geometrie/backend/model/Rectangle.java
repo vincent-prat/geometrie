92ae8599bfa3ff86f8aa6672bcaa7cb0b3500b69
Vincent Prat <prat.vincent.romain@gmail.com>
2022-03-14 23:41:32 +0100

    Update Rectangle.java

--- backend/src/main/java/com/geometrie/backend/model/Rectangle.java
+++ backend/src/main/java/com/geometrie/backend/model/Rectangle.java
@@ -1,4 +1,32 @@
 package com.geometrie.backend.model;
 
-public class Rectangle {
+public class Rectangle extends Quadrilatere implements FigureGeometriqueInterface{
+    private double coteA;
+    private double coteB;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double coteA, double coteB) {
+        this.coteA = coteA;
+        this.coteB = coteB;
+    }
+
+    public double getCoteA() {
+        return coteA;
+    }
+
+    public void setCoteA(double coteA) {
+        this.coteA = coteA;
+    }
+
+    @Override
+    public double CalculPerimetre() {
+        return 2*(coteA+coteB);
+    }
+
+    @Override
+    public double CalculSurface() {
+        return coteA*coteB;
+    }
 }
